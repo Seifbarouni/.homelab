776e3707fb4caca260c15a5a443288b850016402
Seifbarouni <seif.barouni11@gmail.com>
2025-03-01 13:16:38 +0100

    work: made script more resilient

--- infra/traefik/install-traefik.sh
+++ infra/traefik/install-traefik.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
-kubectl create ns traefik-v2
+#set -e  # Stop script execution on error
+
+log() {
+    echo "$(date +"%Y-%m-%d %H:%M:%S") [INFO] $1"
+}
+
+error_exit() {
+    echo "$(date +"%Y-%m-%d %H:%M:%S") [ERROR] $1"
+    exit 1
+}
+
+output=$(kubectl create ns traefik-v2 2>&1)
+
+if [ $? -ne 0 ]; then
+  log "$output"
+fi
+
 # Install in the namespace "traefik-v2"
-helm install --namespace=traefik-v2 \
-    traefik traefik/traefik
+output=$(helm install --namespace=traefik-v2 --values=values.yml traefik traefik/traefik 2>&1)
+
+
+if [ $? -ne 0 ]; then
+  error_exit "$output"
+fi
+
+log "Traefik was installed successfully!"
