aa4cd524814c64a03124bacf6f88addc956b642f
Seifbarouni <seif.barouni11@gmail.com>
2025-03-01 15:43:00 +0100

    work: added the necessary script to install cert-manager

---
+++ infra/cert-manager/helm/install-cert-manager.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e  # Stop script execution on error
+
+log() {
+    echo "$(date +"%Y-%m-%d %H:%M:%S") [INFO] $1"
+}
+
+error_exit() {
+    echo "$(date +"%Y-%m-%d %H:%M:%S") [ERROR] $1"
+    exit 1
+}
+
+output=$(kubectl apply -f cert-manager.yml 2>&1)
+
+
+if [ $? -ne 0 ]; then
+  error_exit "$output"
+fi
+
+log "Cert-manager was installed successfully!"
+
