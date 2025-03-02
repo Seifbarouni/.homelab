985792f0e5cdfc724f3b543dab296547fa75f40a
Seifbarouni <seif.barouni11@gmail.com>
2025-03-02 13:19:22 +0100

    fix: script errors out in unzip when it finds a file with the same name

--- datree.sh
+++ datree.sh
@@ -4,6 +4,6 @@ set -e
 
 wget https://github.com/datreeio/datree/releases/download/1.9.22-rc/datree-cli_1.9.22-rc_Linux_x86_64.zip
 
-unzip datree-cli_1.9.22-rc_Linux_x86_64.zip
+unzip datree-cli_1.9.22-rc_Linux_x86_64.zip -x README.md LICENSE.md
 
 datree test **/*.yml --only-k8s-files --ignore-missing-schemas --policy-config="datree-policies.yaml"
