9d2d0dd9f7912d10a0556d75f26e7a1be8aebc08
Seifbarouni <seif.barouni11@gmail.com>
2025-03-02 13:26:15 +0100

    fix: script errors

--- datree.sh
+++ datree.sh
@@ -6,4 +6,7 @@ wget https://github.com/datreeio/datree/releases/download/1.9.22-rc/datree-cli_1
 
 unzip datree-cli_1.9.22-rc_Linux_x86_64.zip -x README.md LICENSE.md
 
+mv datree /usr/bin/
+mv datree /usr/local/bin/
+
 datree test **/*.yml --only-k8s-files --ignore-missing-schemas --policy-config="datree-policies.yaml"
