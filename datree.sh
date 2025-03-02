ac8d02a951e9fe2ff220efd99bec0aa021bf34e9
Seif Barouni <seif.barouni11@gmail.com>
2025-03-02 13:34:30 +0100

    debug: put script in multiline step

--- datree.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/datreeio/datree/releases/download/1.9.22-rc/datree-cli_1.9.22-rc_Linux_x86_64.zip
-
-unzip datree-cli_1.9.22-rc_Linux_x86_64.zip -x README.md LICENSE.md
-
-chmod +x datree
-
-datree test **/*.yml --only-k8s-files --ignore-missing-schemas --policy-config="datree-policies.yaml"
