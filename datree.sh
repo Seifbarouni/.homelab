24d72f66d6d5d8598e40a0deea402f45d22ae556
Seif Barouni <seif.barouni11@gmail.com>
2025-03-02 13:32:20 +0100

    debug: error could not find datree executable

--- datree.sh
+++ datree.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-set -e
-
 wget https://github.com/datreeio/datree/releases/download/1.9.22-rc/datree-cli_1.9.22-rc_Linux_x86_64.zip
 
 unzip datree-cli_1.9.22-rc_Linux_x86_64.zip -x README.md LICENSE.md
 
-mv datree /usr/bin/
-mv datree /usr/local/bin/
+chmod +x datree
 
 datree test **/*.yml --only-k8s-files --ignore-missing-schemas --policy-config="datree-policies.yaml"
